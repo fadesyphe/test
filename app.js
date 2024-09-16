9be4d8200aea6c46fc915fa7066c4c7834fdefe8
fadesyphe <pivkin.jura2017@gmail.com>
2024-09-17 01:08:24 +0600

    test1

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const menuButton = document.querySelector("#MENU_BUTTON");
 const menu = document.querySelector(".menu");
-const html = document.querySelector("html")
+const html = document.querySelector("html");
 
 menuButton.addEventListener("click", ()=> {
   html.classList.toggle('active');
   menuButton.classList.toggle('active');
   menu.classList.toggle('active');
+})
+
+const priceButton = document.querySelectorAll('[data-price]');
+priceButton.forEach((element) => {
+  element.addEventListener('click', () => {
+    alert("в будущем ссылка на цены");
+  })
+})
+
+const formButton = document.querySelectorAll('[data-form]');
+formButton.forEach((element) => {
+  element.addEventListener('click', () => {
+    alert("в будущем форма")
+  })
 })
